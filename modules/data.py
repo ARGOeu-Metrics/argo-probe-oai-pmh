43f7611d8f9dc44bdc63cb5119bcc7f0dfce1b8b
Katarina Zailac <katarina.zailac@gmail.com>
2025-03-21 12:56:52 +0100

    Function that fetches XML validation schema

--- modules/data.py
+++ modules/data.py
@@ -8,6 +8,7 @@ SCHEMA_URL = "https://www.openarchives.org/OAI/2.0/OAI-PMH.xsd"
 def _get_data(url, timeout):
     try:
         response = requests.get(url, timeout=timeout)
+        response.raise_for_status()
 
         perfdata = (f"|time={response.elapsed.total_seconds()}s;"
                     f"size={len(response.content)}B")
@@ -28,7 +29,6 @@ def get_xml(url, timeout):
     try:
         response, perfdata = _get_data(url, timeout)
         content_type = response.headers["content-type"]
-        response.raise_for_status()
 
     except RequestException as e:
         raise XMLRequestException(msg=e, title=url)
@@ -46,3 +46,25 @@ def get_xml_schema(schema):
     except FileNotFoundError:
         raise XMLSchemaRequestException(f"File {schema} does not exist")
 
+
+def fetch_xml_schema(url, user_agent, timeout):
+    try:
+        headers = {
+            "accept": "text/html,application/xhtml+xml",
+            "user-agent": user_agent
+        }
+        response = requests.get(url, headers=headers, timeout=timeout)
+        response.raise_for_status()
+
+        return response.content
+
+    except (
+        requests.exceptions.HTTPError,
+        requests.exceptions.ConnectionError,
+        requests.exceptions.RequestException,
+        requests.exceptions.Timeout,
+        requests.exceptions.TooManyRedirects
+    ) as e:
+        raise RequestException(
+            msg=f"Error fetching XML schema {url}: {str(e)}"
+        )
